715293073a2762fb095cc59e1446759621f41ec5
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-10-26 22:48:50 +1100

    cl _ test 3

--- python_dice_website/src/app.py
+++ python_dice_website/src/app.py
@@ -2,12 +2,7 @@ import flask
 
 
 class PythonDiceWebApp:
-    def __init__(
-            self,
-            host: str = "localhost",
-            debug: bool = False,
-            port=5000
-    ):
+    def __init__(self, host: str = "localhost", debug: bool = False, port=5000):
         self._app = flask.Flask(__name__)
         self._debug = debug
         self._host = host
@@ -19,15 +14,8 @@ class PythonDiceWebApp:
             return "Hello World"
 
     def run(self):
-        self._app.run(
-            host=self._host,
-            debug=self._debug,
-            port=self._port)
+        self._app.run(host=self._host, debug=self._debug, port=self._port)
 
 
 if __name__ == "__main__":
-    PythonDiceWebApp(
-        host='0.0.0.0',
-        debug=True,
-        port=5000
-    ).run()
+    PythonDiceWebApp(host="0.0.0.0", debug=True, port=5000).run()
