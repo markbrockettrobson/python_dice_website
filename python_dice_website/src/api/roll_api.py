e6079a752c594b4395efaab7644ac82ffe8ab71a
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-10-27 17:44:28 +1100

    added roll api

---
+++ python_dice_website/src/api/roll_api.py
@@ -0,0 +1,43 @@
+import flask
+import python_dice
+
+import python_dice_website.interface.i_api_type as i_api_type
+
+
+class RollApi(i_api_type.IApi):
+    _HELP_NAME = "roll"
+    _HElP_TEXT = (
+        "rolls a python dice program and returns the int result or passer error"
+    )
+    _ROUTE = "/roll"
+
+    # pylint: disable=unused-variable
+    @staticmethod
+    def add_to_app(flask_app: flask.Flask) -> None:
+        @flask_app.route(RollApi._ROUTE, methods=["POST"])
+        def roll():
+            interpreter = python_dice.PythonDiceInterpreter()
+            request_json = flask.request.get_json()
+            print(f"request_json {str(request_json)}")
+            if request_json and "program" in request_json:
+                program = request_json["program"]
+            else:
+                return f"no json program!"
+            split_program = program.split("\n")
+            try:
+                return str(interpreter.roll(split_program)["stdout"])
+            except Exception as e:
+                print(str(e))
+                return f"{str(e)}"
+
+    @staticmethod
+    def get_name() -> str:
+        return RollApi._HELP_NAME
+
+    @staticmethod
+    def get_help_text() -> str:
+        return RollApi._HElP_TEXT
+
+    @staticmethod
+    def get_route() -> str:
+        return RollApi._ROUTE
