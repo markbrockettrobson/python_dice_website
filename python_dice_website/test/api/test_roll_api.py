006dee179dc7e124054f0f50253087fd949b7a62
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-12-08 19:06:51 +1100

    added get request for roll

--- python_dice_website/test/api/test_roll_api.py
+++ python_dice_website/test/api/test_roll_api.py
@@ -6,7 +6,7 @@ import python_dice_website.src.app as app
 
 
 class TestRollAPI(unittest.TestCase):
-    def test_add(self):
+    def test_add_post(self):
         response = app.APP.test_client().post(
             "/roll",
             data=flask.json.dumps({"program": "1 + 3"}),
@@ -18,7 +18,15 @@ class TestRollAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, "4")
 
-    def test_sub(self):
+    def test_add_get(self):
+        response = app.APP.test_client().get("/roll?program=2%20%2B%203")
+
+        data = response.get_data(as_text=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, "5")
+
+    def test_sub_post(self):
         response = app.APP.test_client().post(
             "/roll",
             data=flask.json.dumps({"program": "ABS(1 - 3)"}),
@@ -30,7 +38,15 @@ class TestRollAPI(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, "2")
 
-    def test_error(self):
+    def test_sub_get(self):
+        response = app.APP.test_client().get("/roll?program=2%20%2D%203")
+
+        data = response.get_data(as_text=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, "-1")
+
+    def test_error_post(self):
         response = app.APP.test_client().post(
             "/roll",
             data=flask.json.dumps({"program": "ABS(1 - 3d30"}),
@@ -44,12 +60,30 @@ class TestRollAPI(unittest.TestCase):
             data, "Ran into a $end ($end) where it wasn't expected, at position None."
         )
 
-    def test_error_two(self):
+    def test_error_get(self):
+        response = app.APP.test_client().get("/roll?program=ABS%281%20%2D%203d30")
+
+        data = response.get_data(as_text=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(
+            data, "Ran into a $end ($end) where it wasn't expected, at position None."
+        )
+
+    def test_error_two_post(self):
         response = app.APP.test_client().post(
             "/roll",
             data=flask.json.dumps({"program": "3d3d0"}),
             content_type="application/json",
         )
+
+        data = response.get_data(as_text=True)
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(data, "(None, SourcePosition(idx=3, lineno=-1, colno=-1))")
+
+    def test_error_two_get(self):
+        response = app.APP.test_client().get("/roll?program=3d3d0")
 
         data = response.get_data(as_text=True)
 
