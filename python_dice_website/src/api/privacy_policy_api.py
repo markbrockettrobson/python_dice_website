9e129e9e61937901fc16e00e7c7e58adaca5be82
MarkBrockettRobson <markbrockettrobson@gmail.com>
2019-12-08 23:40:24 +1100

    added a basic PP

---
+++ python_dice_website/src/api/privacy_policy_api.py
@@ -0,0 +1,28 @@
+import flask
+
+import python_dice_website.interface.i_api_type as i_api_type
+
+
+class PrivacyApi(i_api_type.IApi):
+    _HELP_NAME = "privacy"
+    _HElP_TEXT = "returns the privacy policy of the app"
+    _ROUTE = "/privacy"
+
+    # pylint: disable=unused-variable, broad-except
+    @staticmethod
+    def add_to_app(flask_app: flask.Flask) -> None:
+        @flask_app.route(PrivacyApi._ROUTE, methods=["GET"])
+        def privacy_api():
+            return flask.render_template("PrivacyPolicy.html")
+
+    @staticmethod
+    def get_name() -> str:
+        return PrivacyApi._HELP_NAME
+
+    @staticmethod
+    def get_help_text() -> str:
+        return PrivacyApi._HElP_TEXT
+
+    @staticmethod
+    def get_route() -> str:
+        return PrivacyApi._ROUTE
