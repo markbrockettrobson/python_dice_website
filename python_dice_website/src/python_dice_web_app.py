8f41117d88ae9d817abfaf7f1da636687a1ac06a
markbrockettrobson <markbrockettrobson@gmail.com>
2020-10-03 01:54:32 +1000

    support for https servers

--- python_dice_website/src/python_dice_web_app.py
+++ python_dice_website/src/python_dice_web_app.py
@@ -16,6 +16,7 @@ class PythonDiceWebApp:
         debug: bool = False,
         port: int = int(os.environ.get("PORT", 8080)),
         logging: bool = True,
+        https_server: bool = False,
     ):
         self._app = flask.Flask(__name__)
         self._api = flask_restplus.Api(
@@ -43,6 +44,9 @@ class PythonDiceWebApp:
             if logger:
                 logger.info("adding api %s to app.", api.route)
             api.add_to_app(self._api, self._name_space)
+
+        if https_server:
+            self._api.specs_url = flask.url_for("", _external=True, _scheme="https")
 
     def get_app(self) -> flask.Flask:
         return self._app
